f926557b99d7cc2de500e169b542a2faaccd660c
ronaldo.serra <ronaldo.serra@cencosud.cl>
2024-04-24 09:51:42 -0400

    end functions add task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function agregarTarea() {
     // Obtenemos el valor del campo de la tarea
-    let nuevaTareaTexto = document.getElementById("nuevaTarea").value;
+    let nuevaTareaElemento = document.getElementById("nuevaTarea");
+    let nuevaTareaTexto = nuevaTareaElemento.value;
+
+    console.log(nuevaTareaTexto);
 
     // Validamos que el valor no sea vacío
     if (nuevaTareaTexto === "") {
@@ -8,11 +11,25 @@ function agregarTarea() {
         return
     }
 
-    // Crear el elemento en la lista
+    // Crear el elemento de la lista
     let nuevaTarea = document.createElement("li")
     nuevaTarea.textContent = nuevaTareaTexto + " ";
 
     // Crear botón eliminar
+    let botonEliminar = document.createElement('button');
+    botonEliminar.textContent = "Completar Tarea";
 
-    console.log(nuevaTarea)
+    botonEliminar.onclick = function() {
+        nuevaTarea.remove();
+    }
+
+    // Agregar boton eliminar al elemento de la lista
+    nuevaTarea.appendChild(botonEliminar);
+
+    // Agregar elemento a la lista
+    let listaTareas = document.getElementById("listaTareas");
+    listaTareas.appendChild(nuevaTarea);
+
+    // Limpiar cuadro de texto del nombre de la tarea
+    document.getElementById("nuevaTarea").value = "";
 }
